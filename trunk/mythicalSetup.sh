12738edadbee83fcf7896406b6b181c400d742d0
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-02-28 20:34:14 +0000

    Real Show Stopper

--- trunk/mythicalSetup.sh
+++ trunk/mythicalSetup.sh
@@ -234,7 +234,9 @@ echo " ###Database Settings###">>./mythicalSetup
  		AlternateMovieDir="/home/mythtv/Movies"
  		echo " #AlternateMoveDir will act as a Seccondary move dir if the primary moive dir fails">> ./mythicalSetup
  		echo "AlternateMovieDir='$AlternateMovieDir'" >> ./mythicalSetup
-
+ 		echo " #ShowStopper = Enabled prevents generic shows and unrecognized episodes from being processed">> ./mythicalSetup
+ 		 dialog --title "Unrecognizable programming" --yesorno "Process unrecognized Episodes and Shows?" 8 40
+ 		 test "$?" = 0 && echo " ShowStopper=Disabled">> ./mythicalSetup || echo " ShowStopper=Enabled">> ./mythicalSetup
  		echo " #CommercialMarkup will generate comskip files for recordings when they are moved. Enabled|Disabled">> ./mythicalSetup
  		echo "CommercialMarkup=Enabled" >> ./mythicalSetup
 
