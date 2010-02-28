28fc1c7c2b51631628b8ef3646246833b6c54760
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-02-28 21:14:59 +0000

    changed wording

--- trunk/mythicalSetup.sh
+++ trunk/mythicalSetup.sh
@@ -235,7 +235,7 @@ echo " ###Database Settings###">>./mythicalSetup
  		echo " #AlternateMoveDir will act as a Seccondary move dir if the primary moive dir fails">> ./mythicalSetup
  		echo "AlternateMovieDir='$AlternateMovieDir'" >> ./mythicalSetup
  		echo " #ShowStopper = Enabled prevents generic shows and unrecognized episodes from being processed">> ./mythicalSetup
- 		echo " ShowStopper=Enabled">> ./mythicalSetup
+ 		echo " ShowStopper=Disabled">> ./mythicalSetup || echo " ShowStopper=Enabled">> ./mythicalSetup
  		echo " #CommercialMarkup will generate comskip files for recordings when they are moved. Enabled|Disabled">> ./mythicalSetup
  		echo "CommercialMarkup=Enabled" >> ./mythicalSetup
 
@@ -279,7 +279,7 @@ elif [ $mythtv = 0 ]; then
  
  	echo " #ShowStopper = Enabled prevents generic shows and unrecognized episodes from being processed">> ./mythicalSetup
  	dialog --title "Unrecognizable programming" --yesorno "Process unrecognized Episodes and Shows?" 8 40
- 
+  	test "$?" = "1" && echo " ShowStopper=Disabled">> ./mythicalSetup || echo " ShowStopper=Enabled">> ./mythicalSetup
  	echo " #CommercialMarkup will generate comskip files for recordings when they are moved. Enabled|Disabled">> ./mythicalSetup
  	echo "CommercialMarkup=Disabled" >> ./mythicalSetup
 
