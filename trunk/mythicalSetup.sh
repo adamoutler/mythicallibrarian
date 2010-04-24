72fac35c3439c20546434467f0a484163f86fa8d
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-04-24 06:17:36 +0000

    f

--- trunk/mythicalSetup.sh
+++ trunk/mythicalSetup.sh
@@ -109,7 +109,7 @@ if [ "$DownloadML" = "Latest" ]; then
  	while read line
  	do
 		test "$line" = "########################## USER JOBS############################" && let startwrite=$startwrite+1
- 		if [ $startwrite = 2 ]; then
+ 		if [ $startwrite = 1 ]; then
  			echo -e "$line" >> ./librarian
   	echo $startwrite
  		fi
