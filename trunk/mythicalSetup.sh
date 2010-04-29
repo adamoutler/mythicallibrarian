b4ba9e359453c25c282942b3bb4cd39f45659cd7
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-04-29 02:22:23 +0000

    original "." is good enough

--- trunk/mythicalSetup.sh
+++ trunk/mythicalSetup.sh
@@ -125,10 +125,8 @@ if [ "$DownloadML" = "Latest" ]; then
 
  		if [ $startwrite = 2 ]; then
  			clear
-
-			parsing="$parsing""#"
+			parsing="$parsing""."
 			echo $parsing
- 			echo -e "$line"
  			echo -e "$line" >> ./librarian
  		fi
   	done <./mythicalLibrarian.sh
