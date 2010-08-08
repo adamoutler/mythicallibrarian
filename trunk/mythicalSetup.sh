b5d8d65c7365dec762c5e8bc55cb2154babcd1f6
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-08-08 15:03:44 +0000

    checks for oldOutputLog

--- trunk/mythicalSetup.sh
+++ trunk/mythicalSetup.sh
@@ -507,12 +507,13 @@ echo " #DailyReport provides a local log of shows added to your library per day.
 echo "DailyReport=Enabled">> ./myhticalPrep
 echo "#Enables debug mode.  This is a verbose mode of logging which should be used for troubleshooting.  Enabled|Disabled" >> ./myhticalPrep 
 
-test -f "~/.mythicalLibrarian/output.log.old" && OldOutputLog="~/.mythicalLibrarian/output.log.old" || OldOutputLog=""
+OldOutputLog="~/.mythicalLibrarian/output.log.old"
+test ! -f "$OldOutputLog" &&  || OldOutputLog=""
 if [ OldOutputLog != "" ]; then
-	FileSize=$(stat -c%s "$OldOutputLog")
-	test $FileSize > 50000 && echo "DEBUGMODE=Enabled" >> ./myhticalPrep || echo "DEBUGMODE=Disabled" >> ./myhticalPrep
+	FileSize=`stat -c%s "$OldOutputLog"`
+	test $FileSize > 50000 && echo "DEBUGMODE=Disabled" >> ./myhticalPrep || echo "DEBUGMODE=Enabled" >> ./myhticalPrep
 else
- 	echo "DEBUGMODE=Disabled" >> ./myhticalPrep
+ 	echo "DEBUGMODE=Enabled" >> ./myhticalPrep
 fi
 
 
