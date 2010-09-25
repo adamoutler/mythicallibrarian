15fbf5620eb8aefdd57d797c8204f03d20fc8730
outleradam <outleradam@hotmail.com>
2010-09-25 18:57:50 +0000

    Auto run user job

--- mythicalSetup.sh
+++ mythicalSetup.sh
@@ -715,6 +715,8 @@ if [ "$mythtv" = "1" ]; then
   echo ADDING JOB to slot $SlotToUse
   if [ "$SlotToUse" != "0" ]; then
    CheckForTableEntry=`mysql -u$MySQLuser -p$MySQLpass -e "use mythconverg; select * from settings where value like 'UserJob$SlotToUse';"`
+   mysql - -u$MySQLuser -p$MySQLpass -e "use mythconverg; UPDATE settings SET data='1' WHERE value='AutoRunUserJob$SlotToUse'";
+
    test "$CheckForTableEntry" = "" && mysql -u$MySQLuser -p$MySQLpass -e "use mythconverg; INSERT into settings SET value = 'UserJob$SlotToUse';"
    mythicalcheck=`mysql -u$MySQLuser -p$MySQLpass -e "use mythconverg; UPDATE settings SET data='/usr/local/bin/mythicalLibrarian \"%DIR%/%FILE%\"' WHERE value='UserJob$SlotToUse'; UPDATE settings SET data='mythicalLibrarian' WHERE value='UserJobDesc$SlotToUse'; UPDATE settings SET data='1' WHERE value='JobAllowUserJob$SlotToUse';"`
      mysql -u$MySQLuser -p$MySQLpass -e "use mythconverg; UPDATE settings SET data='1' WHERE value='DeletesFollowLinks';"
