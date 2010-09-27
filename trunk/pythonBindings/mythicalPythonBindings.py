3bd7f4411a3ce58ed47d46f22db75eb214e62f74
outleradam@hotmail.com <outleradam@hotmail.com@57e8bce8-f828-11de-9e29-7d1cd92887ef>
2010-09-27 00:05:35 +0000

    added notes

--- trunk/pythonBindings/mythicalPythonBindings.py
+++ trunk/pythonBindings/mythicalPythonBindings.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python
 
-filename = "1004_20100823003000.mpg"
 
-backendIP = "127.0.0.1"
-databaseName = "mythconverg"
-username = "mythtv"
-password = "mythtv"
 
-from MythTV import MythDB
+ #import as command line option
+ filename = "1004_20100823003000.mpg"
 
-db = MythDB(DBHostName=backendIP, DBName=databaseName, DBUserName=username, DBPassword=password)
 
-test = db.getRecorded(chanid=filename[:4],starttime=filename[5:-4])
+ #Possible to add this as a command line option for remote hosts?
+ #This data can be obtained from /home/mythtv/.mythtv/mysql.txt but how to get to it?  
+ #chgrp mythtv /etc/mythtv/mysql.txt in mythicalSetup.  This script could be run with sudo.
+ #sudo grep DBHostName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBHostName='//g
+ backendIP = "127.0.0.1"
+ #sudo grep DBName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBName='//g
+ databaseName = "mythconverg"
+ #sudo grep DBUserName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBUserName='//g
+ username = "mythtv"
+ #sudo grep DBPassword= /home/mythtv/.mythtv/mysql.txt| sed s/'DBPassword='//g
+ password = "mythtv"
+ #The problem is that this data cannot be brought in from the file without a reduction in system permissions.
 
-print "Title = " + test.title
-print "Episode = " + test.subtitle
-print "ChanID = " + str(test.chanid)
-print "ProgramID = " + test.programid
-print "Zap2ItSeriesID = " + test.seriesid
-print "Plot = " + test.description
-print "ShowStartTime = " + str(test.progstart)
-print "ShowCategory = " + test.category
-print "OriginalAirDate = " + str(test.originalairdate)
-#print "XMLTVGrabber = " + ????
-#print "MovieAirDate = " + ????
-print "--- COMSKIP DATA ---"
-print "markup start"
-print test.markup
+ from MythTV import MythDB
+ db = MythDB(DBHostName=backendIP, DBName=databaseName, DBUserName=username, DBPassword=password)
+ test = db.getRecorded(chanid=filename[:4],starttime=filename[5:-4])
+
+
+
+ #Import all information from database regarding file
+ print "Title = " + test.title
+ print "Episode = " + test.subtitle
+ print "ChanID = " + str(test.chanid)
+ print "ProgramID = " + test.programid
+ print "Zap2ItSeriesID = " + test.seriesid
+ print "Plot = " + test.description
+ print "ShowStartTime = " + str(test.progstart)
+ print "ShowCategory = " + test.category
+ print "OriginalAirDate = " + str(test.originalairdate)
+ #print "XMLTVGrabber = " + ????
+ #print "MovieAirDate = " + ????
+ print "--- COMSKIP DATA ---"
+ print "markup start"
+ print test.markup
 
 
 
