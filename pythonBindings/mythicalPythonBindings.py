de11ebb78e20d5fd821c65277294efcf2e8df98b
szczys <szczys@gmail.com>
2010-09-28 01:12:06 +0000

    filename passed as argument, database login pulled from mysql.txt

--- pythonBindings/mythicalPythonBindings.py
+++ pythonBindings/mythicalPythonBindings.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python
 
+import sys, os
+filename = sys.argv[1]
+#filename = "2151_20100923200000.mpg"
+
+#Read database settings from ~/.mythtv/mysql.txt
+mysqlTXT = os.path.expanduser('~') + "/.mythtv/mysql.txt"
+
+with open(mysqlTXT,'r') as f:
+	mysqlData = f.read()
+
+lines = mysqlData.split('\n')
+
+for line in lines:
+    line = line.replace(' ','') #strip spaces
+    if line.startswith('DBHostName'):
+        DBHostName = line.split('=')[1]
+    if line.startswith('DBUserName'):
+        DBUserName = line.split('=')[1]
+    if line.startswith('DBPassword'):
+        DBPassword = line.split('=')[1]
+    if line.startswith('DBName'):
+        DBName = line.split('=')[1]
 
 
-#import as command line option
-filename = "2151_20100923200000.mpg"
-
-
-#Possible to add this as a command line option for remote hosts?
-#This data can be obtained from /home/mythtv/.mythtv/mysql.txt but how to get to it?  
-#chgrp mythtv /etc/mythtv/mysql.txt in mythicalSetup.  This script could be run with sudo.
-#sudo grep DBHostName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBHostName='//g
-backendIP = "192.168.1.105"
-#sudo grep DBName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBName='//g
-databaseName = "mythconverg"
-#sudo grep DBUserName= /home/mythtv/.mythtv/mysql.txt| sed s/'DBUserName='//g
-username = "mythtv"
-#sudo grep DBPassword= /home/mythtv/.mythtv/mysql.txt| sed s/'DBPassword='//g
-password = "mythtv"
-#The problem is that this data cannot be brought in from the file without a reduction in system permissions.
-
+#Get data from mythtv database
 from MythTV import MythDB
-db = MythDB(DBHostName=backendIP, DBName=databaseName, DBUserName=username, DBPassword=password)
+db = MythDB(DBHostName=DBHostName, DBName=DBName, DBUserName=DBUserName, DBPassword=DBPassword)
 test = db.getRecorded(chanid=filename[:4],starttime=filename[5:-4])
 
 
@@ -36,23 +42,8 @@ with open("showData.txt", 'w') as f:
     for x in test.items():
         f.write(x[0] + " = " + str(x[1]) + "\n")
 
-        
-
-'''
-print "Title = " + test.title
-print "Episode = " + test.subtitle
-print "ChanID = " + str(test.chanid)
-print "ProgramID = " + test.programid
-print "Zap2ItSeriesID = " + test.seriesid
-print "Plot = " + test.description
-print "ShowStartTime = " + str(test.progstart)
-print "ShowCategory = " + test.category
-print "OriginalAirDate = " + str(test.originalairdate)
-#print "XMLTVGrabber = " + ????
-#print "MovieAirDate = " + ????
-print "--- COMSKIP DATA ---"
-print "markup start"
-print test.markup
-'''
+#TODO: Commercial skip information
+#markup = test.markup
+#for data in markup
 
 
